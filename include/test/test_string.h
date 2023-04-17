a3d08d120c2446263eb7315ecd7ec9e762dbbf47
Jack <jack4shihong@gmail.com>
2023-04-17 15:55:19 +0800

    add test_string

---
+++ include/test/test_string.h
@@ -0,0 +1,22 @@
+/**
+ * @file test_string.h
+ * @author Shihong Wang (jack4shihong@gmail.com)
+ * @brief `test_string.h`是`string`的测试文件
+ * @version 0.1
+ * @date 2023-04-17
+ * 
+ * @copyright Copyright Shihong Wang (c) 2023 with GNU Public License V3.0
+ */
+
+#ifndef __INCLUDE_TEST_TEST_STRING_H
+#define __INCLUDE_TEST_TEST_STRING_H
+
+#include "string.h"
+
+/**
+ * @brief `test_string`是`string`库的测试函数
+ * @return int 测试正常则返回0
+ */
+int test_string(void);
+
+#endif
---
+++ test/test_string.c
@@ -0,0 +1,16 @@
+/**
+ * @file test_string.c
+ * @author Shihong Wang (jack4shihong@gmail.com)
+ * @brief `test_string.h`是`string`的测试文件
+ * @version 0.1
+ * @date 2023-04-17
+ * 
+ * @copyright Copyright Shihong Wang (c) 2023 with GNU Public License V3.0
+ */
+
+#include "test/test_string.h"
+
+int test_string(void){
+    uart_puts("=> "), uart_puts(__func__), uart_puts(": \n");
+    // TODO 完成该函数
+}
