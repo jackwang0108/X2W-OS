c4a7966258420e5ca5310ab4254e1151e6aeab72
Xiao Zhuiri <xzr3356142450@qq.com>
2023-04-19 02:53:53 +0800

    finish string-test

--- test/test_string.c
+++ test/test_string.c
@@ -12,9 +12,34 @@
 
 // 声明外部符号, 避免include
 extern void uart_puts(const char*);
+extern size_t kprintf(const char* format, ...);
 
 int test_string(void){
     uart_puts("=> "), uart_puts(__func__), uart_puts(": \n");
-    // TODO 完成该函数
+    // TODO 已经完成string的测试：
+    char s[1024]="123456789qwertyuiop@#$$&*())\0";
+    char t[1024]="eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee\0";
+    char* dst = "ffffffffffffffffffffffff\0"; 
+    char* src = "tttttttttttttttttttttttt\0";
+
+    size_t lm;
+    lm=memset(s,'@',19);
+    kprintf("\ttest memset within bound:%s:return val=%d;\n",s,lm);
+    memcpy(t,s,strlen(s)/2);
+    kprintf("\ttest memcpy within bound%s;no return val\n",t);
+    kprintf("\ttest memcmp within bound%d\n",memcmp(s,t,20));
+    kprintf("\ttest strcpy wthin bound %s;\n",strcpy(s,t));
+    int8_t test5 = memcmp("abbbbbbbbbc","abbbbbbbbbd",6);
+    kprintf("\ttest memcmp,return num:%d \n",test5);
+    strcat(dst,src);
+    kprintf("\ttest strcat:%s\n",dst);
+    //test strchr
+    kprintf("\ttest strchr:%s\n",strchr("abbc",'b'));
+    //test strrchr
+    kprintf("\ttest strrchr:%s\n",strrchr("abbc\0",'b'));
+    //test strchrs
+    size_t test6 = strchrs("abbbbc",'b');
+    kprintf("\ttest strchrs:%d\n",test6);
+
     return -1;
 }
