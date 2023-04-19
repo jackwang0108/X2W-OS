d9923be72b1174631e33e6f1a7eaa743c1f73c7c
Jack <jack4shihong@gmail.com>
2023-04-19 16:24:32 +0800

    add

---
+++ test/test_strap.c
@@ -0,0 +1,25 @@
+/**
+ * @file test_strap.h
+ * @author Shihong Wang (jack4shihong@gmail.com)
+ * @brief `test_strap.c`是`strap.c`的测试文件
+ * @version 0.1
+ * @date 2023-04-18
+ * 
+ * @copyright Copyright Shihong Wang (c) 2023 with GNU Public License V3.0
+ */
+
+#include "test/test_strap.h"
+#include "kernel/kstdio.h"
+
+// 使用汇编指令触发异常, 定义在test_strap-asm.S中
+extern NO_RETURN void trigger_breakpoint(void);                         // 触发breakpoint异常
+extern NO_RETURN void trigger_ecall(void);                              // 触发U-ecall异常
+DEPRECATED extern NO_RETURN void trigger_load_access_fault(void);       // 触发Load Access Fault异常, 已弃用
+
+NO_RETURN int test_exception(void){
+    kprintf("=> %s:\n", __func__);
+    // 由于开启了PMP为全地址, 因此不会触发该异常, 已经弃用
+    // trigger_load_access_fault();
+    trigger_breakpoint();
+    trigger_ecall();
+}
