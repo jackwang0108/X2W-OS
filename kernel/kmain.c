b616e1ba18658c0020636c26bf65514f0e3e3120
Jack <jack4shihong@gmail.com>
2023-05-17 19:30:36 +0800

    removed assert

--- kernel/kmain.c
+++ kernel/kmain.c
@@ -22,18 +22,6 @@ void kernel_main(void){
     kprintf("In kernel!\n");
     kprintf("Kernel init!\n");
 
-	// 测试assert宏
-	int one = 1;
-	ASSERT(one == 1, "var one not equals to 1!");
-
-	int a = 1, b = 1;
-	ASSERT(a == b, "var a=%d, b=%d", a, b);
-
-	int* c = &a;
-	// c 被错误初始化为NULL
-	c = NULL;
-	ASSERT(c != NULL, "c=%#016x shouldn't be NULL", c);
-
 	// 初始化内核
     kinit_all();
 
